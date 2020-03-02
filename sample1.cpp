4a96e07010cd9f3719f0486851e2ee963e638328
professor-4 <akashkushwah1607@gmail.com>
2020-03-02 13:28:24 +0530

    My project

---
+++ sample1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 100
+int main(){
+	int page[MAX];
+	int memo_size, page_size, num_pages;
+	char ch='n';
+	int logic_add, physical_add, frame_number, offset_num;
+	printf("\n Enter the size of the memory");
+	scanf("%d",&memo_size);
+	printf("\n Enter the size of each page");
+	scanf("%d", &page_size);
+	num_pages = memo_size/page_size;
+	for(int i=0;i<num_pages;i++)
+	{page[i]=-1;}
+	printf("\n Enter the details of the page table \n");
+	printf("\npage number\t frame number\n--------\t--------");
+}
